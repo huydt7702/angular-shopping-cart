f67165628e7b6c90fc555b36103db7bbf4d26019
huydt7702 <vanhuy.developer@gmail.com>
2023-05-16 15:33:49 +0700

    build html template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CartHeaderComponent } from './cart-header/cart-header.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { PromoCodeComponent } from './promo-code/promo-code.component';
+import { CartSummaryComponent } from './cart-summary/cart-summary.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, CartHeaderComponent, ProductListComponent, PromoCodeComponent, CartSummaryComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
