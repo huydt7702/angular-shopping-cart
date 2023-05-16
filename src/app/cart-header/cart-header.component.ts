b621ba5c83eba703dd430169e429161494fdb399
HuyNguyen <developer.vanhuy@gmail.com>
2023-05-16 16:07:22 +0700

    data binding #1

--- src/app/cart-header/cart-header.component.ts
+++ src/app/cart-header/cart-header.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class CartHeaderComponent {
   title = 'Shopping Cart';
+  numberItems = 4;
 }
--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -3,8 +3,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent {
-
+  products = [
+    {
+      name: 'PRODUCT ITEM NUMBER 1',
+      description: 'Description for product item number 1',
+      thumbnail: 'https://via.placeholder.com/200x150',
+      price: 5.99,
+      quantity: 1,
+    },
+    {
+      name: 'PRODUCT ITEM NUMBER 2',
+      description: 'Description for product item number 2',
+      thumbnail: 'https://via.placeholder.com/200x150',
+      price: 9.99,
+      quantity: 1,
+    },
+  ];
 }
